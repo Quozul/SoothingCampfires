90fe0f9e005dc6f98bae85719848bea6024322bf
Quozul <30729291+Quozul@users.noreply.github.com>
2025-05-09 13:18:28 +0200

    feat: update supported version range for 1.21.2-1.21.5

--- src/main/java/dev/quozul/campfire/mixin/CampfireBlockEntityMixin.java
+++ src/main/java/dev/quozul/campfire/mixin/CampfireBlockEntityMixin.java
@@ -7,10 +7,13 @@ import net.minecraft.block.entity.CampfireBlockEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.recipe.CampfireCookingRecipe;
+import net.minecraft.recipe.ServerRecipeManager;
+import net.minecraft.recipe.input.SingleStackRecipeInput;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.GameRules;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,7 +24,7 @@ import java.util.List;
 @Mixin(CampfireBlockEntity.class)
 public class CampfireBlockEntityMixin {
     @Inject(method = "litServerTick", at = @At("RETURN"))
-    private static void addCampfireRegenerationEffect(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci, @Local boolean bl) {
+    private static void addCampfireRegenerationEffect(ServerWorld world, BlockPos pos, BlockState state, CampfireBlockEntity blockEntity, ServerRecipeManager.MatchGetter<SingleStackRecipeInput, CampfireCookingRecipe> recipeMatchGetter, CallbackInfo ci, @Local boolean bl) {
         if (bl) {
             GameRules gameRules = world.getGameRules();
             int radius = gameRules.getInt(SoothingCampfires.CAMPFIRE_REGENERATION_RADIUS);
